e5dbfcd0b3cf3b83e84b2fb2c838700425e47cd0
Dennis Burger <dennis@dutchwebworks.nl>
2018-06-09 19:37:18 +0200

    Working version of store mutations and login

--- store/index.js
+++ store/index.js
@@ -1,23 +1,29 @@
-export const state = () => {
-	username: null
+import Vuex from "vuex";
+
+const createStore = () => {
+	return new Vuex.Store({
+		state: {
+			username: null
+		},
+		getters: {
+			username(state) {
+				return state.username;
+			},
+			isAuthenticated(state) {
+				return state.username != null;
+			}
+		},
+		mutations: {
+			login(vuexContext, username) {
+				vuexContext.username = username;
+				this.$router.push("/dashboard");
+			},
+			logout(vuexContext) {
+				vuexContext.username = null;
+				this.$router.push("/");
+			}
+		}
+	});
 }
 
-export const getters = {
-	username(state) {
-		return state.username;
-	},
-	isAuthenticated(state) {
-		return state.username != null;
-	}
-}
-
-export const mutations = {
-	login(vuexContext, username) {
-		vuexContext.username = username;
-		this.$router.push("/dashboard");
-	},
-	logout(vuexContext) {
-		vuexContext.username = null;
-		this.$router.push("/");
-	}
-}
+export default createStore;
