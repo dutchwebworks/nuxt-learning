5f7cbe6b24cc5952d1c6222ece3b5e142a0cb148
Dennis Burger <dennis@dutchwebworks.nl>
2019-12-07 16:04:00 +0100

    Added iOS icons to HTML head

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,17 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { href: "/icons/iphone5_splash.png", media: "(device- width: 320px) and(device - height: 568px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/iphone6_splash.png", media: "(device- width: 375px) and(device - height: 667px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/iphoneplus_splash.png", media: "(device- width: 621px) and(device - height: 1104px) and(-webkit - device - pixel - ratio: 3)", rel: "apple - touch - startup - image" },
+      { href: "/icons/iphonex_splash.png", media: "(device- width: 375px) and(device - height: 812px) and(-webkit - device - pixel - ratio: 3)", rel: "apple - touch - startup - image" },
+      { href: "/icons/iphonexr_splash.png", media: "(device- width: 414px) and(device - height: 896px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/iphonexsmax_splash.png", media: "(device- width: 414px) and(device - height: 896px) and(-webkit - device - pixel - ratio: 3)", rel: "apple - touch - startup - image" },
+      { href: "/icons/ipad_splash.png", media: "(device- width: 768px) and(device - height: 1024px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/ipadpro1_splash.png", media: "(device- width: 834px) and(device - height: 1112px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/ipadpro3_splash.png", media: "(device- width: 834px) and(device - height: 1194px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" },
+      { href: "/icons/ipadpro2_splash.png", media: "(device- width: 1024px) and(device - height: 1366px) and(-webkit - device - pixel - ratio: 2)", rel: "apple - touch - startup - image" }
     ]
   },
 
