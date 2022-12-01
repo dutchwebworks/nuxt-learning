901a4b2571bb644388771973eb61cbd0c7645f07
Dennis Burger <dennis.burger@poort80.nl>
2022-12-01 15:44:19 +0100

    Updated header and footer file

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,21 @@
 <template>
     <footer>
-        &copy; Dutchwebworks
+        <div>&copy; Dutchwebworks</div>
+        <div><a href="https://github.com/dutchwebworks/nuxt-learning" target="_blank">View on Github</a></div>
     </footer>
 </template>
+
+<style>
+footer {
+    margin-top: 10px;
+    padding-top: 10px;
+    border-top: 1px solid #ddd;
+    display: flex;
+    justify-content: space-between;
+    font-family: sans-serif;
+}
+
+footer  a {
+    color: deeppink;
+}
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,17 @@
 <template>
     <header>
-        Welcome to my test site
+        <div>Welcome to my test site</div>
+        <div><img src="/icon.png" alt="NuxtJS" width="64" height="64" /></div>
     </header>
 </template>
+
+<style>
+header {
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px dotted #444;
+    font-family: sans-serif;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
