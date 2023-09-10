a63c10ab507aee3209ef5d50941ed6cb5a3cd7c6
Dennis Burger <dennis@dutchwebworks.nl>
2023-09-10 09:29:29 +0200

    Updated footer and logo

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,21 +1,29 @@
 <template>
     <footer>
-        <div>&copy; Dutchwebworks</div>
-        <div><a href="https://github.com/dutchwebworks/nuxt-learning" target="_blank">View on Github</a></div>
+        <span>&copy; Dutchwebworks, August 2022</span>
+        
+        <a href="https://github.com/dutchwebworks/nuxt-learning" target="_blank">
+            View on Github
+
+            <img src="/img/icons/github-logo.svg" alt="Github">
+        </a>
     </footer>
 </template>
 
 <style>
 footer {
-    margin-top: 10px;
-    padding-top: 10px;
-    border-top: 1px solid #ddd;
     display: flex;
     justify-content: space-between;
-    font-family: sans-serif;
+    border-top: 1px solid #ddd;
+    padding-block-start: 10px;
+    margin-block-start: 20px;
+    font-size: .8rem;
 }
 
-footer  a {
+footer > a {
+    display: flex;
+    gap: 5px;
+    align-items: center;
     color: deeppink;
 }
 </style>
